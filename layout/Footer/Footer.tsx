b087ee85ab6a79d0f45da56a9271e34e1b3e0a05
serghv <falko.serega2016@yandex.ru>
2021-11-21 10:53:37 +0300

    grid template

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -2,10 +2,18 @@ import { FooterProps } from './Footer.props';
 import styles from './Footer.module.css';
 import cn from 'classnames';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ className,...props }: FooterProps): JSX.Element => {
 	return (
-		<div {...props}>
-			Footer
-		</div>
+		<footer className={cn(className, styles.footer)} {...props}>
+			<div>
+				OwlTop © 2020 - 2021 Все права защищены
+			</div>
+			<div>
+				Пользовательское соглашение
+			</div>
+			<div>
+				Политика конфиденциальности
+			</div>
+		</footer>
 	);
 };
