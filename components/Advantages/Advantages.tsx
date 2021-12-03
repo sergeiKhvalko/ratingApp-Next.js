5dec3f2d60d7a802f21f397a6783d4a6d015a34b
serghv <falko.serega2016@yandex.ru>
2021-12-03 21:20:35 +0300

    layout advantages

---
+++ components/Advantages/Advantages.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { AdvantagesProps } from './Advantages.props';
+import styles from './Advantages.module.css';
+import CheckIcon from './check.svg';
+
+
+export const Advantages = ({ advantages }: AdvantagesProps): JSX.Element => {
+	console.log(advantages);
+	return (
+		<>
+			{
+				advantages.map(a => (
+					<div key={a._id} className={styles.advantage}>
+						<CheckIcon/>
+						<div className={styles.title}>{a.title}</div>
+						<hr className={styles.vline} />
+						<div>{a.description}</div>
+					</div>
+				))
+			}
+		</>
+	);
+};
--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,11 +1,12 @@
 import { TopPageComponentProps } from './TopPageComponent.props';
 import styles from './TopPageComponent.module.css';
 import React from 'react';
-import { HhData, Htag, Tag } from '../../components';
+import { Advantages, HhData, Htag, P, Tag } from '../../components';
 import { TopLevelCategory } from '../../interfaces/page.interface';
 
 
 export const TopPageComponent = ({ page, products, firstCategory}: TopPageComponentProps): JSX.Element => {
+
 	return (
 		<div className={styles.wrapper}>
 			<div className={styles.title}>
@@ -24,6 +25,17 @@ export const TopPageComponent = ({ page, products, firstCategory}: TopPageCompon
 			</div>
 
 			{ firstCategory == TopLevelCategory.Courses && page.hh && <HhData {...page.hh} /> }
+			
+			{
+				page.advantages && page.advantages.length > 0 && <>
+					<Htag tag='h2'>Преимущства</Htag>
+					<Advantages advantages={page.advantages} />
+				</>
+			}
+
+			{ page.seoText && <P>{page.seoText}</P> }
+			<Htag tag='h2'>Получаемые навыки</Htag>
+			{ page.tags.map(t => <Tag key={t} color={'primary'}>{t}</Tag>) }
 		</div>
 	);
 };
